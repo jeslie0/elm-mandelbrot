fb8121aa0be81377a53a4e94f10e6912ba54f228
jeslie0 <jamesleslie@posteo.net>
2023-07-30 20:56:36 +0000

    Deploying to gh-pages from @ jeslie0/elm-mandelbrot@331dda34142ff1eab6ff48ce0279c65ce34b459c 🚀

---
+++ canvas.js
@@ -0,0 +1,64 @@
+let canvasElement;
+let ctx;
+
+let canvasHeight;
+let canvasWidth;
+
+let imageData;
+
+const addRow = (rowData) => {
+    const { row, computedColours } = rowData;
+
+    const offset = 4 * row * computedColours.length;
+
+    for (let i = 0; i < computedColours.length; i++) {
+        const { red, blue, green, alpha } = computedColours[i]
+        const scaledIdx = 4 * i + offset
+        imageData.data[scaledIdx + 0] = 255 * red
+        imageData.data[scaledIdx + 1] = 255 * green
+        imageData.data[scaledIdx + 2] = 255 * blue
+        imageData.data[scaledIdx + 3] = 255 * alpha
+    }
+
+    // Draw image data to the canvas
+    ctx.putImageData(imageData, 0, 0);
+}
+
+const addRows = (rowsData) => {
+    const helper = (_acc, rowData) => {
+        const { row, computedColours } = rowData;
+
+        const offset = 4 * row * computedColours.length;
+
+        for (let i = 0; i < computedColours.length; i++) {
+            const { red, blue, green, alpha } = computedColours[i]
+            const scaledIdx= 4 * i + offset
+            imageData.data[scaledIdx + 0] = 255 * red
+            imageData.data[scaledIdx + 1] = 255 * green
+            imageData.data[scaledIdx + 2] = 255 * blue
+            imageData.data[scaledIdx + 3] = 255 * alpha
+        }
+    }
+
+    rowsData.reduce(helper, undefined);
+    ctx.putImageData(imageData, 0, 0);
+}
+
+const setInitialSettings = (settings) => {
+    const {height, width, canvasId} = settings
+
+    canvasHeight = height
+    canvasWidth = width
+
+    canvasElement = document.getElementById(canvasId);
+    ctx = canvasElement.getContext("2d");
+    imageData = ctx.createImageData(width, height);
+
+    app.ports.settingsSet.send(undefined);
+}
+
+
+
+app.ports.sendRow.subscribe(addRow)
+app.ports.sendRows.subscribe(addRows)
+app.ports.sendInitialSettings.subscribe(setInitialSettings)
